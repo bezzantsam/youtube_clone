ea69c9afe44e1f1b8fa875c7da141d21d61885ab
Sam <bezzantsam@gmail.com>
2022-08-30 06:47:44 -0700

    search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
   <Stack
@@ -18,6 +19,7 @@ const Navbar = () => (
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
   </Stack>
 );
 
