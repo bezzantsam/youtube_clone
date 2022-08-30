64d482b45ed8b0cf81c17e0f844890fc7f9ae1d1
Sam <bezzantsam@gmail.com>
2022-08-30 07:02:26 -0700

    box with border outline

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,19 @@ import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
-  return <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}></Stack>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Hi
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
