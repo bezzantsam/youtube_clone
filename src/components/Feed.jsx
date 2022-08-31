a7aa658c17f541f0dcdf5165e17b1dad8fd50060
Sam <bezzantsam@gmail.com>
2022-08-31 02:30:26 -0700

    Sidebar imported

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
@@ -11,7 +12,14 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        Hi
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 Sam Bezzant Media
+        </Typography>
       </Box>
     </Stack>
   );
