d8bf4206076cbf0548e0568ed40066ccb0e6973c
Sam <bezzantsam@gmail.com>
2022-08-31 02:37:21 -0700

    categories mapped and rendered

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import { Stack } from "@mui/material";
 
-const Sidebar = () => {
-  return;
-  <div>Sidebar</div>;
-};
+import { categories } from "../utils/constants";
+
+const Sidebar = () => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button>
+        <span>{category.icon}</span>
+        <span>{category.name}</span>
+      </button>
+    ))}
+  </Stack>
+);
 
 export default Sidebar;
