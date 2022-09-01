8b654b983f17e41dc0b56604260ccc81e35fa2f6
Sam <bezzantsam@gmail.com>
2022-09-01 06:52:12 -0700

    searchfeed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search results for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
