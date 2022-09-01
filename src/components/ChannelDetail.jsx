4f7aba5271314229a00ffe3c5da8d034d5108d18
Sam <bezzantsam@gmail.com>
2022-09-01 05:25:09 -0700

    ChannelDetail linear gradient, Box returned in jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(
+        `search?channelId=${id}&part=snippet%2Cid&order=date`
+      );
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(55,23,138,1) 0%, rgba(19,47,150,1) 53%, rgba(31,179,209,1) 100%)",
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
