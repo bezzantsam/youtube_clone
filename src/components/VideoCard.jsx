9842980320363bf3ffba3ac65dba115a5165361f
Sam <bezzantsam@gmail.com>
2022-09-01 03:01:15 -0700

    Video card subtitle

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -33,7 +33,13 @@ const VideoCard = ({
           sx={{ width: 358, height: 180 }}
         />
       </Link>
-      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }} />
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
