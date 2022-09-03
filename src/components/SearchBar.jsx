cf185ade2ab6e62fa98c75fbf8d76c1e8c71f04b
Sam <bezzantsam@gmail.com>
2022-09-03 01:20:23 -0700

    SearchBar input logic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,7 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState(second);
   return (
     <Paper
       component="form"
@@ -19,8 +20,8 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
         <Search />
