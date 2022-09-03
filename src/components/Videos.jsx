9f859e8d2db9f89b0be447d25cc19cb0617760e6
Sam <bezzantsam@gmail.com>
2022-09-03 04:08:56 -0700

    mvp

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading....";
   return (
     <Stack
       direction={direction || "row"}
