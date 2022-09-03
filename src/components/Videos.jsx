faac94d1cbc271590ea67a0c0dc85065c9d97e4f
Sam <bezzantsam@gmail.com>
2022-09-03 03:59:33 -0700

    suggested videos in column within Stack

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos?.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
